4a8c68205896a0aec4729ae8d7d2ff75ef96ca6d
Abin <abin@luminescent.digital>
2020-04-30 16:06:04 +0530

    Create user model and checking validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-console.log("user")
-//@route GET api/user
-//@desc Test route
+//@route Post api/user
+//@desc Register user
 //@access Public
+router.post(
+	'/',
+	[
+		check('name', 'Name is required').not().isEmpty(),
+		check('email', 'Please enter a valid email').isEmail(),
+		check(
+			'password',
+			'Please enter password with minimum 6 characters',
+		).isLength({ min: 6 }),
+	],
+	(req, res) => {
+        
+        const errors = validationResult(req);
 
-router.get('/', (req,res) => {
-    res.send("User route");
-})
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()});
+        }
+
+		res.send('User route');
+	},
+);
 
 module.exports = router;
