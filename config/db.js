c6675d01722f25f21d0b1a4933de33ba9630eb7b
Abin <abin@luminescent.digital>
2020-04-30 17:18:01 +0530

    Create user register with encrypted password and gravatar

--- config/db.js
+++ config/db.js
@@ -2,17 +2,19 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-
 const connectDB = async () => {
-    try {
-       await  mongoose.connect(db,{ useNewUrlParser: true, useUnifiedTopology: true });
-       console.log("Mongodb Connected")
-        
-    } catch (err) {
-        console.log("Error mesg :", err.message)
-        //Eixt process with failure
-        process.exit(1)
-    } 
-}
+	try {
+		await mongoose.connect(db, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+		console.log('Mongodb Connected');
+	} catch (err) {
+		console.log('Error mesg :', err.message);
+		//Eixt process with failure
+		process.exit(1);
+	}
+};
 
 module.exports = connectDB;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const gravatar = require('gravatar');
 const { check, validationResult } = require('express-validator/check');
+const User = require('../../models/User');
 
 //@route Post api/user
 //@desc Register user
@@ -15,13 +18,39 @@ router.post(
 			'Please enter password with minimum 6 characters',
 		).isLength({ min: 6 }),
 	],
-	(req, res) => {
-        
-        const errors = validationResult(req);
+	async (req, res) => {
+		const { name, email, password } = req.body;
 
-        if(!errors.isEmpty()){
-            return res.status(400).json({errors: errors.array()});
-        }
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		try {
+			//Checking is user exist
+			let user = await User.findOne({ email });
+			if (user) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: 'user alredy exists' }] });
+			}
+			//Set gravatar
+            const avatar = gravatar.url(email, { s: '200', r: 'pg', d: 'rm' });
+            
+            user = new User({name, email,password,avatar});
+
+            //Encrypt password
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+            res.send("User registered");
+            
+		} catch (err) {
+			console.log(err.message);
+			return res.send(500).send('Server error');
+		}
 
 		res.send('User route');
 	},
