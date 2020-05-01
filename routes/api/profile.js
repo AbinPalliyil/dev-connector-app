d921228df5a9081abfd4b4381fed2a89c0fb57b9
Abin <abin@luminescent.digital>
2020-05-01 14:44:13 +0530

    Create api for fetch user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth')
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 
 //@route GET api/profile
 //@desc Test route
 //@access Public
 
-router.get('/', (req,res) => {
-    route})
+router.get('/me', auth, async (req,res) => {
+    try {
+        const profile = await Profile.findOne({user: req.user.id}).populate(
+            'user',
+            ['name', 'avatar']
+        );
+        if(!profile){
+            return res.status(400).json({msg: "There is no profile for this user"});
+        }
+        res.json(profile);
+    } catch (err) {
+        console.log(err.message);
+        return res.status(500).send("Server side error")
+        
+    }
+})
 
 module.exports = router;
