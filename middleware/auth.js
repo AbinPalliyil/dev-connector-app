074be8a169ff18ae222a92856d4e51588b40aa91
Abin <abin@luminescent.digital>
2020-05-01 11:14:35 +0530

    Fetch authenticated user

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req, res, next){
+    // Get the token from header
+    const token = req.header('x-auth-token');
+
+    // Cheeck if no token
+    if(!token){
+        return res.status(401).json({msg: "No token , authorization denined"});
+    }
+    // Verify token
+    try {
+        const decode = jwt.verify(token, config.get('jwtToken'));
+        req.user = decode.user;
+        next();
+    } catch (error) {
+        return res.status(401).json({msg: "Invalid token"});
+    }
+
+
+}
