99b2d3043169c6e36dac0c860b2800225c851127
Abin <abin@luminescent.digital>
2020-05-04 11:35:06 +0530

    Create Route for client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 import React, {Fragment} from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = () => 
+<Router>
   <Fragment>
     <Navbar />
-    <Landing />
+  <Route path="/" exact component={Landing} />
+
+  <section class="container">
+  <Switch>
+  <Route path="/login" exact component={Login} />
+  <Route path="/register" exact component={Register} />
+</Switch>
+  </section>
   </Fragment>
+
+  </Router>
 
 
 export default App;
