40abf25825ce0975ee038a34133b4337dfa1b2a1
Abin <abin@luminescent.digital>
2020-05-04 13:13:17 +0530

    create redux store to the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
-import React, {Fragment} from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import { Provider } from 'react-redux';
+import store from './store';
 
-const App = () => 
-<Router>
-  <Fragment>
-    <Navbar />
-  <Route path="/" exact component={Landing} />
+const App = () => (
+	<Provider store={store}>
+		<Router >
+			<Fragment>
+				<Navbar />
+				<Route path='/' exact component={Landing} />
 
-  <section className="container">
-  <Switch>
-  <Route path="/login" exact component={Login} />
-  <Route path="/register" exact component={Register} />
-</Switch>
-  </section>
-  </Fragment>
-
-  </Router>
-
+				<section className='container'>
+					<Switch>
+						<Route path='/login' exact component={Login} />
+						<Route path='/register' exact component={Register} />
+					</Switch>
+				</section>
+			</Fragment>
+		</Router>
+	</Provider>
+);
 
 export default App;
